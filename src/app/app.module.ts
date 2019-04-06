8a83a8e438ffa151fffb389f6f3e999c89012c66
Nicholas Sumshchenko <maybe@gmail.com>
2019-04-06 15:47:43 +0300

    changes added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { RoutesControllersModule } from './routesControllersModule/routes-controllers-module';
+import { RoutesControllersComponent } from './routesControllersModule/routes-controllers/routes-controllers.component';
+
+const appRoutes: Routes = [
+  { path: '', component: RoutesControllersComponent }
+];
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RoutesControllersModule
+    RoutesControllersModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
